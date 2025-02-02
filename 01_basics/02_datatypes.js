c80468e2b6267d1c64478e89a098f48cad823f40
YusraNoor332 <yusranoor332@gmail.com>
2025-02-02 20:03:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS as newer version (telling the engine)
+
+//alert(3+3)   //we are using node js not browser
+
+//code readability matters alot although the javascript balances the spaces by its own
+
+console.log("yusra");
+
+//tc39.es and MDN for javascript standard documentation
+
+let name ="yusra"
+let age = 20
+let isLoggedIn = false
+
+//number=> 2 to power 53
+//bigint is used in trading or stockmarket
+//string => ""
+//bolean => true/false
+//null => standalone value (representation of empty value but not undefined)
+//undefined => the value is not assigned yet
+//symbol => to find uniqueness between components
+
+//object => 
+
+console.log(typeof "yusra");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+    
+
+ 
