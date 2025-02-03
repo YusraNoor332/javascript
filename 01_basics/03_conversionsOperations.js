897dfb3f8a757e006316a9d4c217aed2c925d8fb
YusraNoor332 <yusranoor332@gmail.com>
2025-02-03 18:57:18 +0000

    conversions

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -11,20 +11,49 @@ let score = "33"   //number
 //this value will be converted on number but it wont be accurate it will NaN (not a number)
 //NaN is also a special type
 
-console.log(typeof score);
+//console.log(typeof score);
 let valueInNumber = Number(score)//conversion of string into number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-console.log(typeof NaN); //type of NaN is number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof NaN); //type of NaN is number
 
 let isLoggedIn = 1 //true
 //let isLoggedIn = 0  //false
 //let isLoggedIn = "yusra"  //true
 //let isLoggedIn = ""  //false
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 let someNumber =33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//************************** Operations ************************ 
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2-/);
+// console.log(2%2);  //use in cryptography
+
+let str1 = "hello"
+let str2 = " yusra"
+let str3 = str1 + str2 //concatenation of string
+console.log(str3);
+
+console.log("1" + 2);  //12
+console.log(1 + "2");  //12
+console.log("1" + 2 +2);  //122
+console.log(1 + 2 + "2");   //32
+/*in case of JS conversions its a rule that if the
+first one is string then others will be treated as
+string also and if the string is at the end then the starting 
+operations will be performed */ 
+
+
+
